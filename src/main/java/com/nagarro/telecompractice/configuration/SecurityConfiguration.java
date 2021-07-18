77376e2971d10f4f39322a3cb27d41d0cce27a5c
Deepak Yadav <dy79402@gmail.com>
2021-07-18 19:18:09 +0530

    added security configuration class.

--- src/main/java/com/nagarro/telecompractice/configuration/SecurityConfiguration.java
+++ src/main/java/com/nagarro/telecompractice/configuration/SecurityConfiguration.java
@@ -1,14 +1,20 @@
-//package com.nagarro.telecompractice.configuration;
-//
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//	}
-//
-//}
+package com.nagarro.telecompractice.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	UserDetailsService  userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+}
---
+++ src/main/java/com/nagarro/telecompractice/serviceImpl/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.nagarro.telecompractice.serviceImpl;
+
+public class UserDetailsServiceImpl {
+
+}
